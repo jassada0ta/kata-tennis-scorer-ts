c88953f26f592432c1472955f3a28563a2e78fd5
Jassada(Ta) <jassada.p@thoughtworks.com>
2020-03-23 15:01:41 +0700

    refactor to use string interpolation

--- src/TennisScorer.ts
+++ src/TennisScorer.ts
@@ -1,10 +1,10 @@
 export class TennisScorer {
     callScore(p1Score: number, p2Score: number): string {
-        const scoreMap :any = {
-            0:"love",
-            15:"fifteen"
+        const scoreMap: any = {
+            0: "love",
+            15: "fifteen"
         };
-        return scoreMap[p1Score]+"-all";
+        return `${scoreMap[p1Score]}-all`;
     }
 
 }

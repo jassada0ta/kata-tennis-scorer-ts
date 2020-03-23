41f516a69fa3f913435d8013be5ea5673aad715b
Jassada(Ta) <jassada.p@thoughtworks.com>
2020-03-23 14:59:04 +0700

    refactor to use scoreMap to translate score

--- src/TennisScorer.ts
+++ src/TennisScorer.ts
@@ -1,9 +1,10 @@
 export class TennisScorer {
     callScore(p1Score: number, p2Score: number): string {
-        if(p1Score===15){
-            return "fifteen-all";
-        }
-        return "love-all";
+        const scoreMap :any = {
+            0:"love",
+            15:"fifteen"
+        };
+        return scoreMap[p1Score]+"-all";
     }
 
 }

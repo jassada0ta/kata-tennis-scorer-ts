3bffe11783a4a1f5fa94c5660a5c41b68d8f1ab6
Jassada(Ta) <jassada.p@thoughtworks.com>
2020-03-24 09:11:01 +0700

    refactor test to be data driven

--- src/TennisScorer.test.ts
+++ src/TennisScorer.test.ts
@@ -2,110 +2,31 @@ import { TennisScorer } from './TennisScorer';
 import { expect } from 'chai';
 
 describe("TennisScorer", () => {
-    let scorer: TennisScorer;
-    beforeEach(() => {
-        scorer = new TennisScorer();
-    });
-    it("should create new scorer object", () => {
-        expect(scorer).to.not.be.undefined;
-    });
 
     describe("callScore", () => {
-        let result: string;
-        let p1Score: number;
-        let p2Score: number;
 
-        const act = () => {
-            result = scorer.callScore(p1Score, p2Score);
-        };
+        const tests: any[] = [
+            [0, 0, "love-all"],
+            [15, 15, "fifteen-all"],
+            [15, 0, "fifteen-love"],
+            [30, 0, "thirty-love"],
+            [40, 0, "forty-love"],
+            [40, 40, "deuce"],
+            [41, 40, "advantage player1"],
+            [40, 41, "advantage player2"]
+        ];
 
-        describe("when score is 0-0", () => {
-            beforeEach(() => {
-                p1Score = 0;
-                p2Score = 0;
-            });
-
-            it("should return love-all", () => {
-                act();
-                expect(result).to.equal("love-all");
+        tests.forEach(t => {
+            const p1Score = t[0];
+            const p2Score = t[1];
+            const expectedResult = t[2];
+            describe(`when score is ${p1Score}-${p2Score}`, () => {
+                it(`should return ${expectedResult}`, () => {
+                    const scorer = new TennisScorer();
+                    const result = scorer.callScore(p1Score, p2Score);
+                    expect(result).to.equal(expectedResult);
+                });
             });
         });
-        describe("when score is 15-15", () => {
-            beforeEach(() => {
-                p1Score = 15;
-                p2Score = 15;
-            });
-
-            it("should return fifteen-all", () => {
-                act();
-                expect(result).to.equal("fifteen-all");
-            });
-        });
-        describe("when score is 15-0", () => {
-            beforeEach(() => {
-                p1Score = 15;
-                p2Score = 0;
-            });
-
-            it("should return fifteen-love", () => {
-                act();
-                expect(result).to.equal("fifteen-love");
-            });
-        });
-        describe("when score is 30-0", () => {
-            beforeEach(() => {
-                p1Score = 30;
-                p2Score = 0;
-            });
-
-            it("should return thirty-love", () => {
-                act();
-                expect(result).to.equal("thirty-love");
-            });
-        });
-        describe("when score is 40-0", () => {
-            beforeEach(() => {
-                p1Score = 40;
-                p2Score = 0;
-            });
-
-            it("should return forty-love", () => {
-                act();
-                expect(result).to.equal("forty-love");
-            });
-        });
-        describe("when score is 40-40", () => {
-            beforeEach(() => {
-                p1Score = 40;
-                p2Score = 40;
-            });
-
-            it("should return deuce", () => {
-                act();
-                expect(result).to.equal("deuce");
-            });
-        });
-        describe("when score is 41-40", () => {
-            beforeEach(() => {
-                p1Score = 41;
-                p2Score = 40;
-            });
-
-            it("should return advantage player1", () => {
-                act();
-                expect(result).to.equal("advantage player1");
-            });
-        });
-        describe("when score is 40-41", () => {
-            beforeEach(() => {
-                p1Score = 40;
-                p2Score = 41;
-            });
-
-            it("should return advantage player2", () => {
-                act();
-                expect(result).to.equal("advantage player2");
-            });
-        });
-    })
+    });
 });
